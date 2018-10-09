9769af6a66c72fa47d3bc3f987af5fdcef6db945
MemoryForSky <1365677361@qq.com>
2018-10-09 23:57:05 +0800

    update binary tree @2018.10.9

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -1,5 +1,7 @@
-# 二叉树节点
 class TreeNode:
+    """
+    二叉树节点
+    """
     def __init__(self, x):
         self.val = x
         self.left = None
@@ -7,21 +9,63 @@ class TreeNode:
 
 
 class Solution:
-# 求二叉树的最大深度
-    def max_depth(self):
-        pass
+    """
+    二叉树的求解：递归 + 迭代
+    """
+    def max_depth(self, node):
+        """
+        求二叉树的最大深度
+        """
+        if node is None:
+            return 0
 
-# 求二叉树的最小深度
-    def min_depth(self):
-        pass
+        left = self.max_depth(node.left)
+        right = self.max_depth(node.right)
 
-# 求二叉树中节点的个数
-    def node_count(self):
-        pass
+        return max(left, right) + 1
 
-# 求二叉树中叶子节点的个数
-    def leaf_node_count(self):
-        pass
+    def min_depth(self, node):
+        """
+        求二叉树的最小深度
+        """
+        if node is None:
+            return 0
+
+        left = self.min_depth(node.left)
+        right = self.min_depth(node.right)
+
+        return min(left, right) + 1
+
+
+    def node_count(self, node):
+        """
+        求二叉树中节点的个数
+        :param node:
+        :return:
+        """
+        if node is None:
+            return 0
+
+        left = self.node_count(node.left)
+        right = self.node_count(node.right)
+
+        return left + right + 1
+
+    def leaf_node_count(self, node):
+        """
+        求二叉树中叶子节点的个数
+        """
+        if node is None:
+            return 0
+
+        if node.left is None and node.right is None:
+            return 1
+
+        left = self.leaf_node_count(node.left)
+        right = self.leaf_node_count(node.right)
+
+        return left + right
+
 
 # 求二叉树中第k层节点的个数
     def k_level_count(self):
@@ -82,6 +126,9 @@ class Solution:
 
 
 if __name__ == '__main__':
+    """
+    
+    """
     node_a = TreeNode("a")
     node_b = TreeNode("b")
     node_c = TreeNode("c")
@@ -91,6 +138,7 @@ if __name__ == '__main__':
     node_g = TreeNode("g")
     node_h = TreeNode("h")
     node_i = TreeNode("i")
+    node_k = TreeNode("k")
     node_a.left = node_b
     node_a.right = node_c
     node_b.left = node_d
@@ -99,3 +147,24 @@ if __name__ == '__main__':
     node_c.right = node_g
     node_e.left = node_h
     node_e.right = node_i
+    node_h.left = node_k
+
+    root = node_a
+    S = Solution()
+
+    #-----------------Test-----------------
+    print("****** Test max depth: ******")
+    max_depth = S.max_depth(node_b)
+    print("max depth is: ", max_depth)
+
+    print("****** Test min depth: ******")
+    min_depth = S.min_depth(node_k)
+    print("min depth is: ", min_depth)
+
+    print("****** Test node count: ******")
+    node_count = S.node_count(root)
+    print("node count is: ", node_count)
+
+    print("****** Test leaf node count: ******")
+    leaf_node_count = S.leaf_node_count(root)
+    print("leaf node count is: ", leaf_node_count)
