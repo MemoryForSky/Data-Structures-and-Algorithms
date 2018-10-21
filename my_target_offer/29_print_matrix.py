2da54bdb6f9bfe645062d80c638e1479d2d34d58
MemoryForSky <1365677361@qq.com>
2018-10-21 23:19:22 +0800

    update interview coding @2018.10.21

---
+++ my_target_offer/29_print_matrix.py
@@ -0,0 +1,17 @@
+"""
+
+面试题29：顺时针打印矩阵
+题目：输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+
+"""
+
+class Solution:
+    def print_matrix(self, matrix):
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+
+
+    def print_matrix_in_circle(self, matrix):
+        pass
+
